313579c0be2a099d5033a93a5363204b16d8961a
Progga <proggaroy2000@gmail.com>
2023-02-06 22:00:22 +0600

    find-max-num

---
+++ find-max-num.js
@@ -0,0 +1,23 @@
+
+function findMax(jim, della, chinku) {
+
+    if (jim > della && jim > chinku) {
+        console.log('Jim get the cake')
+    }
+    else if (della > jim && della > chinku) {
+        console.log('Della get the cake')
+    }
+    else {
+        console.log('Chinku get the cake')
+    }
+
+}
+const numbers = findMax(84, 99, 77);
+console.log(numbers);
+
+function findMax(jim, della, chinku) {
+return Math.max(jim,della,chinku)
+
+}
+const numbers1 = findMax(84, 99, 77);
+console.log(numbers1);
